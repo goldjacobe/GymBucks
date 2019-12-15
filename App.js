187e375c7fd4b87565791ae62774649c1cda1453
Jacob Gold <goldjacobe@gmail.com>
2019-12-15 17:10:09 -0500

    NOW login working

--- App.js
+++ App.js
@@ -1,16 +1,39 @@
 import React, { useState } from "react";
-import { View, TextInput, Button } from "react-native";
+import { View, TextInput, Text, Button } from "react-native";
 import Navigator from "./components/navigators/TabNavigator";
+import apigClientFactory from "./apig/apigClient"
 
 export default function App() {
   const [loggedIn, setLoggedIn] = useState(false);
-  const [phone, setPhone] = useState("");
-  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+  const [phone, setPhone] = useState("3106223581");
+  const [password, setPassword] = useState("123");
   const [confirm, setConfirm] = useState("");
   const [name, setName] = useState("");
 
   const logIn = () => {
-    setLoggedIn(true);
+    var apigClient = apigClientFactory.newClient({
+      apiKey: "hp3cPqP6Ml9jTtt579YcH7qzQkDtBUUJ4QdQlq7A"
+    });
+    var params = {
+      phone: phone,
+      password: password
+    };
+    apigClient
+      .signinGet(params)
+      .then(function(result) {
+        // This is executed if get a 200 response
+        if (result.data) { // Check to make sure that log in was successful
+          setError(false);
+          setLoggedIn(true);
+        } else {
+          setError(true);
+        }
+      })
+      .catch(function(result) {
+        setError(true);
+        console.log(result);
+      });
   };
   const logOut = () => {
     setLoggedIn(false);
@@ -37,6 +60,7 @@ export default function App() {
           style={{margin: 10}}
         />
         <Button title="Sign in" onPress={logIn} />
+        {error && <Text style={{color: 'red'}}>Invalid login</Text>}
       </View>
     )
   }
