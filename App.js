451233b917c5d8ea8104e3116f28bf70e83db86a
Jacob Gold <goldjacobe@gmail.com>
2019-12-15 17:08:20 -0500

    Revert "working login" bc i forgot to include libs
    This reverts commit b9f914831f08f492eb00144f6f8a599a12a14be7.

--- App.js
+++ App.js
@@ -1,39 +1,16 @@
 import React, { useState } from "react";
-import { View, TextInput, Text, Button } from "react-native";
+import { View, TextInput, Button } from "react-native";
 import Navigator from "./components/navigators/TabNavigator";
-import apigClientFactory from "./apig/apigClient"
 
 export default function App() {
   const [loggedIn, setLoggedIn] = useState(false);
-  const [error, setError] = useState(false);
-  const [phone, setPhone] = useState("3106223581");
-  const [password, setPassword] = useState("123");
+  const [phone, setPhone] = useState("");
+  const [password, setPassword] = useState("");
   const [confirm, setConfirm] = useState("");
   const [name, setName] = useState("");
 
   const logIn = () => {
-    var apigClient = apigClientFactory.newClient({
-      apiKey: "hp3cPqP6Ml9jTtt579YcH7qzQkDtBUUJ4QdQlq7A"
-    });
-    var params = {
-      phone: phone,
-      password: password
-    };
-    apigClient
-      .signinGet(params)
-      .then(function(result) {
-        // This is executed if get a 200 response
-        if (result.data) { // Check to make sure that log in was successful
-          setError(false);
-          setLoggedIn(true);
-        } else {
-          setError(true);
-        }
-      })
-      .catch(function(result) {
-        setError(true);
-        console.log(result);
-      });
+    setLoggedIn(true);
   };
   const logOut = () => {
     setLoggedIn(false);
@@ -60,7 +37,6 @@ export default function App() {
           style={{margin: 10}}
         />
         <Button title="Sign in" onPress={logIn} />
-        {error && <Text style={{color: 'red'}}>Invalid login</Text>}
       </View>
     )
   }
