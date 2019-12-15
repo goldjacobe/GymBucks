e33e791723e58b1ed66e1fb852dd245849556e54
Jacob Gold <goldjacobe@gmail.com>
2019-12-15 16:02:59 -0500

    added fields

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
-import { View, Text, Button } from "react-native";
+import { View, TextInput, Button } from "react-native";
 import Navigator from "./components/navigators/TabNavigator";
 
 export default function App() {
   const [loggedIn, setLoggedIn] = useState(false);
+  const [phone, setPhone] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirm, setConfirm] = useState("");
+  const [name, setName] = useState("");
+
   const logIn = () => {
     setLoggedIn(true);
   };
@@ -13,10 +18,26 @@ export default function App() {
   if (loggedIn) {
     return (
       <View style={{flex: 1}}>
-        <Navigator screenProps={{hello: "hello", what: 1, logOut: logOut}}/>
+        <Navigator screenProps={{phone: phone, logOut: logOut}}/>
       </View>
     );
   } else {
-    return <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}><Button title="Sign in" onPress={logIn}></Button></View>
+    return (
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <TextInput
+          value={phone}
+          onChangeText={setPhone}
+          placeholder={'Phone'}
+          style={{margin: 10}}
+        />
+        <TextInput
+          value={password}
+          onChangeText={setPassword}
+          placeholder={'Password'}
+          style={{margin: 10}}
+        />
+        <Button title="Sign in" onPress={logIn} />
+      </View>
+    )
   }
 };
