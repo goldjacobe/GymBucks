44fa1a269c284b0816aa996ad91fc62ed4ebaada
Jacob Gold <goldjacobe@gmail.com>
2019-12-15 00:55:29 -0500

    added schedule to schedule screen and dates below circles

--- components/schedule/ScheduleRow.js
+++ components/schedule/ScheduleRow.js
@@ -2,17 +2,18 @@ import React from "react";
 import { View, Text, Dimensions } from "react-native";
 import _ from "lodash"
 
-
 const circSize = Dimensions.get('window').width/10;
+const millisInDay = 86400000
 const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 
 class ScheduleRow extends React.Component {
     render() {
         return (
-        <View style={{ flex: 0, height: 2*circSize, margin: circSize/4 }}>
-            <Text>Schedule</Text>
+        <View style={{ flex: 0, height: 2*circSize, margin: circSize/4, justifyContent: "space-between" }}>
+            <Text style={{fontWeight: "bold"}}>Schedule</Text>
             <View style={{ flex: 1, flexGrow: 1, justifyContent: "space-between", alignItems: "center", flexDirection: "row", }}>
-                {_.map(days, this.getCircle)}
+                {_.map(days, this.getCircle.bind(this))}
             </View>
         </View>);
     }
@@ -23,12 +24,23 @@ class ScheduleRow extends React.Component {
                 <View style={{ alignItems: "center", justifyContent: "center",width: circSize, height: circSize, borderRadius: circSize/2, backgroundColor: 'green'}}>
                     <Text>{day[0]}</Text>
                 </View>
-                <Text>{num}</Text>
-
-                
+                <Text>{this.getDay(num)}</Text>
             </View>
         )
     }
+
+    getDay(num) {
+        const now = new Date(Date.now() + millisInDay*num)
+        const date = now.getDate();
+        if (num == 0 || date == 1) {
+            return months[now.getMonth()] + " " + date;
+        }
+        return date;
+    }
 }
+
+ScheduleRow.defaultProps = {
+    week: 0
+};
 
 export default ScheduleRow;
--- components/screens/ScheduleScreen.js
+++ components/screens/ScheduleScreen.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { View, Text } from "react-native";
+import ScheduleRow from "../schedule/ScheduleRow";
 
 class ScheduleScreen extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Schedule!</Text>
+      <View style={{
+        flex: 1,
+        justifyContent: "flex-start",
+        alignItems: "stretch",
+        backgroundColor: "red"
+      }}>
+        <ScheduleRow />
       </View>
     );
   }
