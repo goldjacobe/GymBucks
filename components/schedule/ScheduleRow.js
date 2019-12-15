881b2330698e7d76eb8c7d058f6de9ad2526425d
Jacob Gold <goldjacobe@gmail.com>
2019-12-15 01:11:14 -0500

    add prev and next weeks

--- components/schedule/ScheduleRow.js
+++ components/schedule/ScheduleRow.js
@@ -3,19 +3,25 @@ import { View, Text, Dimensions } from "react-native";
 import _ from "lodash"
 
 const circSize = Dimensions.get('window').width/10;
+const daysInWeek = 7
 const millisInDay = 86400000
 const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
 const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+const weeks = ["Last week", "This week", "Next week"]
 
 class ScheduleRow extends React.Component {
     render() {
         return (
         <View style={{ flex: 0, height: 2*circSize, margin: circSize/4, justifyContent: "space-between" }}>
-            <Text style={{fontWeight: "bold"}}>Schedule</Text>
+            <Text style={{fontWeight: "bold"}}>{this.getWeek()}</Text>
             <View style={{ flex: 1, flexGrow: 1, justifyContent: "space-between", alignItems: "center", flexDirection: "row", }}>
                 {_.map(days, this.getCircle.bind(this))}
             </View>
         </View>);
+    }
+
+    getWeek() {
+        return weeks[this.props.week+1]
     }
 
     getCircle(day, num) {
@@ -30,7 +36,7 @@ class ScheduleRow extends React.Component {
     }
 
     getDay(num) {
-        const now = new Date(Date.now() + millisInDay*num)
+        const now = new Date(Date.now() + millisInDay*(num+daysInWeek*this.props.week))
         const date = now.getDate();
         if (num == 0 || date == 1) {
             return months[now.getMonth()] + " " + date;
