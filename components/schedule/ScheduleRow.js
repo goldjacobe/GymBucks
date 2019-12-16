2a7ca0242a5a0cef8e824d618d97f194aa40136d
Jacob Gold <goldjacobe@gmail.com>
2019-12-16 02:40:39 -0500

    Scheduling works!

--- components/schedule/ScheduleRow.js
+++ components/schedule/ScheduleRow.js
@@ -2,6 +2,8 @@ import React from "react";
 import { View, Text, Dimensions } from "react-native";
 import _ from "lodash"
 import apigClientFactory from "../../apig/apigClient"
+import { TouchableHighlight } from "react-native-gesture-handler";
+import { ScenesReducer } from "react-navigation-stack";
 
 const circSize = Dimensions.get('window').width/10;
 const daysInWeek = 7
@@ -49,13 +51,41 @@ class ScheduleRow extends React.Component {
         return weeks[this.props.week+1]
     }
 
+    schedule(stamp) {
+        var sched = this.state.schedule;
+        sched.push ({"date": stamp})
+        this.setState({schedule: sched})
+
+        var apigClient = apigClientFactory.newClient({
+            apiKey: "hp3cPqP6Ml9jTtt579YcH7qzQkDtBUUJ4QdQlq7A"
+          });
+        var params = {
+            uid : this.props.uid,
+            date : stamp
+        };
+        apigClient
+        .addinpoolPost({},params,{})
+        .then(function(result) {   
+            console.log(result)
+        })
+        .catch(function(result) {
+            console.log(result);
+        });
+    }
+    
     getCircle(day, num) {
         const time = this.getTime(num);
+        const stamp = this.getStamp(time);
+        const color = this.getColor(stamp);
+        var inside = <View style={{ alignItems: "center", justifyContent: "center",width: circSize, height: circSize, borderRadius: circSize/2, backgroundColor: color}}>
+            <Text>{day[0]}</Text>
+        </View>
+        if (this.props.week == 1) {
+            inside = <TouchableHighlight style={{borderRadius: circSize/2}}onPress={() => this.schedule(stamp)}>{inside}</TouchableHighlight>
+        }
         return (
             <View style={{ flex: 1, flexGrow: 1, alignItems: "center"}} key={day}>
-                <View style={{ alignItems: "center", justifyContent: "center",width: circSize, height: circSize, borderRadius: circSize/2, backgroundColor: this.getColor(time)}}>
-                    <Text>{day[0]}</Text>
-                </View>
+                {inside}
                 <Text>{this.getText(time, num)}</Text>
             </View>
         )
@@ -75,17 +105,20 @@ class ScheduleRow extends React.Component {
         return date;
     }
 
-    getColor(time) {
-        if (this.state.schedule.length == 0) {
-            return 'grey'
-        }
+    getStamp(time) {
         var month = time.getMonth() + 1
         month = month < 10 ? "" + 0 + month : "" + month
         var date = time.getDate()
         date = date < 10 ? "" + 0 + date : "" + date
         var year = time.getYear()
         year += 1900
-        const stamp = month + "/" + date + "/" + year
+        return month + "/" + date + "/" + year
+    }
+
+    getColor(stamp) {
+        if (this.state.schedule.length == 0) {
+            return 'grey'
+        }
         for (var i = 0; i < this.state.schedule.length; i++) {
             const sched = this.state.schedule[i]['date']
             if (sched == stamp) {
