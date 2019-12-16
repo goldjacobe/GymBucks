4b0d5ed0c511f336b4f8deff7f0eda6fb19b5749
Ed Shao <ss5902@columbia.edu>
2019-12-15 22:46:41 -0500

    get profile

--- components/screens/ProfileScreen.js
+++ components/screens/ProfileScreen.js
@@ -7,7 +7,12 @@ import ProfileTile from "../profile/ProfileTile";
 import SettingList from "../profile/SettingList";
 
 const ProfileScreen = props => {
-  const [userData, setuserData] = useState({});
+  const [userData, setuserData] = useState({
+    balance: 10000,
+    completed_workout: 100,
+    curpollnum: 0,
+    curpollbal: 0
+  });
   console.log("PROFILE SCREEN PROPS: ", props);
   get_user(props.screenProps.uid, setuserData);
 
@@ -19,8 +24,8 @@ const ProfileScreen = props => {
           <ProfileTile
             style={styles.profileTile}
             profileData={{
-              title: "Total Earning",
-              value: "$132.00",
+              title: "Total Money In Pool ",
+              value: userData.curpollbal.toString(),
               iconName: "md-trophy",
               iconColor: "#ea8e50"
             }}
@@ -65,6 +70,41 @@ const ProfileScreen = props => {
   );
 };
 
+function update_user() {
+  var query = document.getElementById("uid").value;
+
+  var apigClient = apigClientFactory.newClient({
+    apiKey: "hp3cPqP6Ml9jTtt579YcH7qzQkDtBUUJ4QdQlq7A"
+  });
+
+  //you need to input all the current info to update this user in the database
+  var params = {
+    uid: query,
+    name: "Ashley",
+    username: "ashleywu",
+    phone: "3106223581",
+    email: "tw2725@columbia.edu",
+    profilepic: "default.png",
+    balance: 10000,
+    completed_workout: 100,
+    curpollnum: 0,
+    curpollbal: 0
+  };
+  apigClient
+    .updateinfoPut({}, params, {})
+    .then(function(result) {
+      //returned response
+      // {
+      // "statusCode": 200,
+      // "body": "\"success\""
+      // }
+      show_result(result.data);
+    })
+    .catch(function(result) {
+      console.log(result);
+    });
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -108,8 +148,6 @@ const get_user = (query, setuserData) => {
       //   "curpollnum":0,
       //   "curpollbal":0
       // }
-      console.log("RESULT: ", result);
-      console.log(result.data);
       setuserData(result.data);
     })
     .catch(function(result) {
